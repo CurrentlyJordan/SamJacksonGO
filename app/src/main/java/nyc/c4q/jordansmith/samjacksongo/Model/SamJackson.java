48037a72807f8a4944dc2a05b601f3edce9a2e43
Jose Vila <you@example.comJDVila@gmail.com>
2016-12-18 14:47:56 -0500

    added a few new Sam's

--- app/src/main/java/nyc/c4q/jordansmith/samjacksongo/Model/SamJackson.java
+++ app/src/main/java/nyc/c4q/jordansmith/samjacksongo/Model/SamJackson.java
@@ -114,7 +114,7 @@ public class SamJackson implements Serializable {
 
     public SamJackson randomTransform() {
         Random random = new Random();
-        int randNumber = random.nextInt(4) + 1;
+        int randNumber = random.nextInt(6) + 1;
         switch (randNumber) {
             case 1:
                 return frozone();
@@ -124,6 +124,10 @@ public class SamJackson implements Serializable {
                 return mrGlass();
             case 4:
                 return nickFury();
+            case 5:
+                return ordellRobbie();
+            case 6:
+                return stephen();
             default:
                 return frozone();
         }
@@ -144,10 +148,10 @@ public class SamJackson implements Serializable {
         SamJackson samJackson = new SamJackson();
         samJackson.name = "Jules";
         samJackson.movie = "Pulp Fiction";
-        samJackson.imageUrl = "http://img10.deviantart.net/44e2/i/2014/356/6/8/pulp_fiction_by_wahyuajisadewa-d8asbg1.jpg";
+        samJackson.imageUrl = "http://img.cinemablend.com/cb/0/b/a/a/c/f/0baacf08003ab795356460f39ceb5c0535babbc15846c57942a6c4215fa2ac01.jpg";
         samJackson.gifUrl = "http://giphy.com/gifs/quentin-tarantino-pulp-fiction-samuel-l-jackson-3otPoz2V9lPlSH9pde";
         samJackson.deleteMessage = "Time to get a Royale with cheese";
-        samJackson.quote = "DO I LOOK LIKE A BITCH!";
+        samJackson.quote = "DO I LOOK LIKE A BITCH?!";
         return samJackson;
     }
 
@@ -173,6 +177,27 @@ public class SamJackson implements Serializable {
         return samJackson;
     }
 
+    public SamJackson ordellRobbie() {
+        SamJackson samJackson = new SamJackson();
+        samJackson.name = "Ordell Robbie";
+        samJackson.movie = "Jackie Brown";
+        samJackson.imageUrl = "https://cdn.pastemagazine.com/www/system/images/photo_albums/samuel-l-jackson/large/25-jackson-jackiebrown.jpg?1384968217";
+        samJackson.gifUrl = "http://namtab.com/writing/MMjackiebrown02.gif";
+        samJackson.deleteMessage = "Are we going to Roscoe's?";
+        samJackson.quote = "My ass may be dumb, but I ain't no dumbass...";
+        return samJackson;
+    }
+
+    public SamJackson stephen() {
+        SamJackson samJackson = new SamJackson();
+        samJackson.name = "Stephen Candee";
+        samJackson.movie = "Django Unchained";
+        samJackson.imageUrl = "https://pdjeliclark.files.wordpress.com/2013/01/sam-jackson-as-stephen-in-django.jpg";
+        samJackson.gifUrl = "http://i.imgur.com/B2lVCvc.gif";
+        samJackson.deleteMessage = "You can't delete me Motherf##kah, I've been here forever!";
+        samJackson.quote = "Aw, naw, naw. I ain't got no problem with it. If you ain't got no problem with burnin' the bed, the sheets, the pillowcase, and everything else...";
+        return samJackson;
+    }
 
 }
 
